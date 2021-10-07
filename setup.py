37919300e581843b3d94b76836e7e6d6d8b5a04b
RajeshThakur1 <rajeshthakur1r@gmail.com>
2021-10-08 00:00:01 +0530

    added the setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="RajeshThakur1",
+    description="A small package for dvc ml pipeline demo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RajeshThakur1/dvc_basic_demo",
+    author_email="rajeshthakur1r@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
